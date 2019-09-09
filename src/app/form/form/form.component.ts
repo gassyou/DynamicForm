06c01e7ff16ca194efd7ee9014037409ff62d43b
unknown <zheng.xiangfei@gmail.com>
2019-09-09 17:00:12 +0800

    add submit function

--- src/app/form/form/form.component.ts
+++ src/app/form/form/form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SFSchema } from '@delon/form';
 import { FormConfigService } from 'src/app/form-config.service';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-form',
@@ -11,10 +12,17 @@ export class FormComponent implements OnInit {
 
   title = 'AutoFormEngine';
   schema: SFSchema = {};
-  constructor( private configService: FormConfigService) { }
+  constructor(
+    private configService: FormConfigService,
+    private msg: NzMessageService
+    ) { }
 
   ngOnInit() {
     this.schema = this.configService.form.getForm();
+  }
+
+  submit(value: any) {
+    this.msg.success(JSON.stringify(value));
   }
 
 }
